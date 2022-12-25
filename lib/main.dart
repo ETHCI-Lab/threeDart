8832db76b5a9a2ecfbfac4daf0c87ff2a71574de
tony0831-L <t0983822437@gmail.com>
2022-12-26 02:19:17 +0800

    login

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:bootstrap_icons/bootstrap_icons.dart';
 import 'home.dart';
-import 'package:flutter/services.dart';
-void main() {
+import 'Fetch.dart';
+import 'login.dart';
 
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]).then((value) =>runApp(const ThreeDa()));
-}
+void main() => runApp(const MyApp());
 
-class ThreeDa extends StatelessWidget{
-
-  const ThreeDa({super.key});
-
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: WebGlLoaderObj(),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('test')),
+        drawer:  Drawer(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(60),
+                bottomRight: Radius.circular(60)
+            ),
+          ),
+          child: Column(
+            children:  [
+              Row(
+                children: const [
+                  Expanded(
+                    flex: 1,
+                    child: UserAccountsDrawerHeader(
+                      accountName: Text("test"),
+                      accountEmail: Text("test@gmail.com"),
+                      currentAccountPicture: CircleAvatar(
+                        backgroundImage: NetworkImage("http://media.discordapp.net/attachments/1051886825142243419/1052240060612890744/1606783962.png"),
+                      ),
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                              fit: BoxFit.fitWidth,
+                              image: NetworkImage("http://cdn.discordapp.com/attachments/1051886825142243419/1053691923526864896/242274964.png")
+                          )
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              const ListTile(
+                title: Text("setProfile"),
+                leading: CircleAvatar(
+                  backgroundColor: Colors.transparent,
+                  child:  Icon(BootstrapIcons.people_fill,color: Color(0XFF263238),),
+                ),
+              ),
+              const Divider(),
+              const ListTile(
+                title: Text("setting"),
+                leading: CircleAvatar(
+                  backgroundColor: Colors.transparent,
+                  child:  Icon(BootstrapIcons.sliders,color: Color(0XFF263238),),
+                ),
+              ),
+              const Divider(),
+            ],
+          ),
+        ),
+        body: const MyStatefulWidget(),
+      ),
     );
   }
-
 }
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  void initState() {
+    Fetch.testServe();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+    ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text("test"),
+            ElevatedButton(
+            style: style,
+            onPressed: (){
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (BuildContext context)=> const WebGlLoaderObj())
+              );
+            },
+            child: const Text('to three'),
+            ),
+            ElevatedButton(
+              style: style,
+              onPressed: (){
+                // login('root','aaaa');
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (BuildContext context)=> const CategoryPage())
+                );
+              },
+              child: const Text('login'),
+            ),
+          ],
+        ),
+      ));
+  }
+}
+
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:testroom/home.dart';
 
 import 'package:testroom/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const ThreeDa());
+    await tester.pumpWidget(const WebGlLoaderObj());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
